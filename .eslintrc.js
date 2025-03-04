c8356abfdcc1f97b0216093c4a3da58fdd63f6dc
JulianChu00 <julianchu626@gmail.com>
2025-03-04 15:10:50 +0800

    feat: 测试

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+  'eslint:recommended',
+  'plugin:vue/vue3-recommended'
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/html-closing-bracket-spacing': 'off',
+    'vue/require-default-prop': 'off',
+    'indent':0,
+    'space-before-function-paren':0
+  }
+}
